8508a3cb75b4d218ef21bbedeedb08e7d8b53c10
elishowk <elishowk@nonutc.fr>
2011-02-09 20:31:37 +0100

    changed index to UT and implemented subrecords

--- easiparse/importer.py
+++ easiparse/importer.py
@@ -3,13 +3,14 @@ import os, sys
 import re
 
 import logging
-logging.basicConfig(level=logging.DEBUG, format="%(levelname)-8s %(message)s")
+logging.basicConfig(level=logging.WARNING, format="%(levelname)-8s %(message)s")
 import os, sys
 import codecs
 import exceptions
 
 from pymongo import Connection
 MONGODB_PORT = 27017
+
 
 def notice_to_file(output_file, notice_lines):
    """
@@ -19,27 +20,64 @@ def notice_to_file(output_file, notice_lines):
       output_file.write( line )
    logging.debug("written %d lines to %s"%(len(notice_lines),output_file))
 
+
 class NoticeRejected(exceptions.Exception):
    pass
 
+
 class Notice(object):
    def __init__(self, config, lines):
+
       self.config = config
       tag_length = config['isi']['tag_length']
       multiline = config['isi']['multiline']
       fields = config['isi']['fields']
-      for line in lines:
+      subfields = config['isi']['subfields']
+      i = 0
+      self.total_lines = len(lines)
+      while 1:
+         if i == self.total_lines: break
+         line = lines[i]
          tag = self.getTag(line)
-         if tag not in fields.keys() and tag != multiline:
+         if tag == "UT":
+            self.__dict__["_id"] = self.getLine(line)
+            i+=1
             continue
-         stripline = line[ tag_length : ].strip()
-         if tag != multiline:
-            last_tag = tag
-            self.appendLine(tag, stripline)
-         else:
-            self.appendLine(last_tag, stripline)
-      self.normalize()
+         if tag not in fields.keys() and tag != multiline:
+            if tag in subfields.keys():
+               i = self.appendSubfield(tag, i, lines)
+               continue
+            else:
+               i+=1
+               continue
+
+         self.dispatchValidLine(tag, line)
+         i+=1
+
+      self.normalize(fields)
       self.filter()
+
+
+   def appendSubfield(self, tag, i, lines):
+      sublines=[]
+      i += 1
+      while 1:
+         if i == self.total_lines:
+            raise NoticeRejected("reached EOF without closing a Notice")
+         subline = lines[i]
+         subtag = self.getTag(subline)
+         if subtag == self.config['isi']['subfields'][tag]['end']:
+            subfield = SubRecord(self.config, tag, sublines).__dict__
+            if tag in self.__dict__.keys():
+               self.__dict__[tag] += [subfield]
+            else:
+               self.__dict__[tag] = [subfield]
+            i+=1
+            return i
+         else:
+            sublines += [subline]
+         i+=1
+      return i
 
    def filter(self):
       """
@@ -48,7 +86,9 @@ class Notice(object):
       match_regexp = self.config['match_regexp']
       required_fields = self.config['required_fields']
       extraction_fields = self.config['extraction_fields']
-
+      self.__delattr__('total_lines')
+      self.__delattr__('last_tag')
+      self.__delattr__('config')
       for tag in required_fields:
          if tag not in self.__dict__:
             raise NoticeRejected("notice incomplete")
@@ -67,8 +107,8 @@ class Notice(object):
       raise NoticeRejected("notice did not match")
       return 0
 
-   def normalize(self):
-      for tag, rule in self.config['isi']['fields'].iteritems():
+   def normalize(self, field_rules):
+      for tag, rule in field_rules.iteritems():
          if tag not in self.__dict__.keys(): continue
          if type(rule) == str:
             self.__dict__[tag] = rule.join(self.__dict__[tag])
@@ -89,7 +129,42 @@ class Notice(object):
       self.__dict__[tag] = self.parseLine(tag, line)
 
    def getTag(self, line):
-      return line[ : self.config['isi']['tag_length'] ].strip()
+      tag_length = self.config['isi']['tag_length']
+      tag = line[ : tag_length ].strip()
+      return tag
+
+   def getLine(self, line):
+      tag_length = self.config['isi']['tag_length']
+      return line[ tag_length : ].strip()
+
+   def dispatchValidLine(self, tag, line):
+      stripline = self.getLine(line)
+      if tag != self.config['isi']['multiline']:
+         self.last_tag = tag
+         self.appendLine(tag, stripline)
+      else:
+         self.appendLine(self.last_tag, stripline)
+
+class SubRecord(Notice):
+   def __init__(self, config, tag, sublines):
+      self.config = config
+      tag_length = config['isi']['tag_length']
+      multiline = config['isi']['multiline']
+      fields = config['isi']['subfields'][tag]['fields']
+      for line in sublines:
+         tag = self.getTag(line)
+         if tag not in fields.keys() and tag != multiline:
+            continue
+         self.dispatchValidLine(tag, line)
+      self.normalize(fields)
+      self.filter()
+
+   def filter(self):
+      """
+      clean unwanted attributes
+      """
+      self.__delattr__('config')
+      self.__delattr__('last_tag')
 
 def main(file_isi, config, limit=None, overwrite=False):
    """
@@ -111,7 +186,7 @@ def main(file_isi, config, limit=None, overwrite=False):
    total_imported = 0
 
    # itere sur les lignes du corpus
-   for line in file_isi.readlines():
+   for line in file_isi:
       if in_notice == 1 and end_tag.match(line) is None:
          file_lines += [line]
       elif begin_tag.match(line) is not None and in_notice == 0:
@@ -128,10 +203,10 @@ def main(file_isi, config, limit=None, overwrite=False):
          except NoticeRejected, nr:
             logging.debug("%s"%nr)
             pass
-         if total_imported >= limit:
+         if limit is not None and total_imported >= limit:
             return total_imported
       else:
-         logging.warning("line skipped : not between notice TAGS")
+         #logging.debug("line skipped : not between notice TAGS")
          continue
 
    return total_imported

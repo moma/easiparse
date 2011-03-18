e1c7d2555fd354c7ad033c8eaca71169f224a14e
elishowk <elishowk@nonutc.fr>
2011-03-18 12:51:06 +0100

    moved main multiprocessing managers to each module

--- easiparse.py
+++ easiparse.py
@@ -14,42 +14,12 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from optparse import OptionParser
 import yaml
-from glob import glob
-import re
 
-from easiparse import importer, output, mongodbhandler, cooccurrences
-
-import codecs
-from multiprocessing import pool
+from easiparse import importer, extractor, cooccurrences
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)-8s %(message)s")
 
-def import_worker(config, input_path):
-    isi_file = codecs.open(input_path, "rU", encoding="ascii", errors="replace")
-    outputs = output.getConfiguredOutputs(config['importer'], input_path)
-    subtotal = importer.main(
-        isi_file,
-        config['importer'],
-        outputs
-    )
-    logging.debug("imported %d matching notices in %s"%(subtotal, isi_file))
-
-
-def extract_worker(config, fieldname):
-    """
-    not modular at all...
-    copies input db notices matching a regexg to an output db
-    """
-    input = mongodbhandler.MongoDB(config['extractor']['input_db']['mongo_host'],\
-            config['extractor']['input_db']['mongo_port'],\
-            config['extractor']['input_db']['mongo_db_name'],\
-            config['extractor']['input_db']['mongo_login'])
-    outputs = output.getConfiguredOutputs( config['extractor'] )
-    reg = re.compile( config['extractor']['filters']['regexp_content']['regexp'], re.I|re.U|re.M)
-    
-    for notice in input.notices.find({ fieldname:{"$regex":reg} }, timeout=False):
-        outputs['mongodb'].save(notice, "notices")
 
 def get_parser():
     parser = OptionParser()
@@ -63,20 +33,10 @@ if __name__ == "__main__":
     config = yaml.load( open( "config.yaml", 'rU' ) )
 
     if options.execute=='import':
-        glob_list = glob(config['importer']['input_path'])
-        pool = pool.Pool(processes=config['processes'])
-        for input_path in glob_list:
-            pool.apply_async(import_worker, (config, input_path))
-            #import_worker(config, input_path)
-        pool.close()
-        pool.join()
+        importer.main_multiprocessing(config)
 
     if options.execute=='extract':
-        pool = pool.Pool(processes=config['processes'])
-        for fieldname in config['extractor']['filters']['regexp_content']['fields']:
-            pool.apply_async(extract_worker, (config, fieldname))
-        pool.close()
-        pool.join()
+        extractor.main(config)
 
     if options.execute=='cooccurrences':
         cooccurrences.main_cooccurrences(config)
